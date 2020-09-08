bb80f35b1f90e1575aec3196c48c336887c532e7
Stefan Vujic <stefan@paramountwebtechnology.com>
2020-09-08 22:12:05 +0100

    pawned amend/code optimization

--- ajax_controller.php
+++ ajax_controller.php
@@ -76,26 +76,45 @@ function call_have_i_been_pawned($email) {
 	return $breach_list;
 }
 
+function get_more_pawned_info($email) {
+	$headers = array(
+    	"hibp-api-key: 356caf020183406f88b0a67e4c445867",
+    	"user-agent: email_investigator",
+	);
+
+	$breaches = curl_response("https://haveibeenpwned.com/api/v3/breachedaccount/", $email . "?truncateResponse=false", $headers);
+	$breaches = json_decode($breaches);
+
+	return $breaches;
+}
+
+
 $email = $_POST["email"];
+$get_more_pawned_info = $_POST["morePawndInfo"];
 
 if ($email) {
-	if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-		$capcha_response = verify_recapcha_v2("6LdmtcAZAAAAAC73DOUkWIK0zAo4wHaK7gJknjMp");
-
-		// if ($capcha_response) {
-			echo json_encode(
-				array(
-					"data" => array(
-						"have_i_been_pawned" => call_have_i_been_pawned($email)
-					),	
-					"scrapers" => array(
-						"google" => scrape_google($email, 2),
-						"bing" => scrape_bing($email)
-					)
-				)
-			);
-		// }
+	if ($get_more_pawned_info) {
+		$breaches = get_more_pawned_info($email);
+		echo json_encode($breaches);
 	}else {
-		echo json_encode("bad email");
+		if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+			$capcha_response = verify_recapcha_v2("6LdmtcAZAAAAAC73DOUkWIK0zAo4wHaK7gJknjMp");
+
+			// if ($capcha_response) {
+				echo json_encode(
+					array(
+						"data" => array(
+							"have_i_been_pawned" => call_have_i_been_pawned($email)
+						),	
+						"scrapers" => array(
+							"google" => scrape_google($email, 2),
+							"bing" => scrape_bing($email)
+						)
+					)
+				);
+			// }
+		}else {
+			echo json_encode("bad email");
+		}
 	}
 }
--- js/app.js
+++ js/app.js
@@ -1,8 +1,6 @@
 $(function() {
 	$("#submit-main-form").on("click", function(){
 
-		var html;
-		html = "";
 		var email = $("#email").val();
 		var loader = "<div class='row' id='loader'><div class='col-sm-12 col-lg'><img src='/assets/loader.gif'/></div></div>";
 
@@ -25,47 +23,34 @@ $(function() {
 				if (controllerData == "bad email") {
 					$("#error-message").append("Please enter a valid email");
 				}else {
-					$.each(controllerData.data.have_i_been_pawned, function(siteKey, siteName) {
-						console.log(siteName);
-					});
-
-					$.each(controllerData.scrapers, function(scraperName, scraperData) {
-						if (scraperData.length == false || controllerData.scrapers.bing[1].length == false) {
-
-							html += "<div class='tab " + scraperName + "-tab'>";
-							html += "<div class='row website-title'>";
-								html += "<div class='col-sm site-title-text'>" + scraperName + "</div>";
-							html += "</div>";
-
-							html += "<div class='col-sm col-lg empty-message'>Nothing found</div>";
-							html += "</div>";
-
-						}else {
-							html += "<div class='tab " + scraperName + "-tab'>";
-							html += "<div class='row website-title'>";
-								html += "<div class='col-sm site-title-text'>" + scraperName + "</div>";
-							html += "</div>";
-
-							$.each(scraperData, function(pageIndex, pageData) {
-
-								$.each(pageData, function(recordIndex, record) {
-									html += "<div class='row record-row'>";
-										html += "<div class='col-sm-12 col-lg record " + scraperName + "-website-url website-url'>" + record.website + "</div>";
-										html += "<div class='col-sm-12 col-lg record " + scraperName + "-title'>" + record.title + "</div>";
-										html += "<div class='col-sm-12 col-lg record " + scraperName + "-url'><a href='" + record.url + "' target='_blank'>" + record.url.substring(0,35) + "...</a></div>";
-									html += "</div>";
-
-									html += "<hr>";
-								});
-
-							});
-							html += "</div>";
-							html += "</div>";
-						}
-					});
+					html = renderResult(controllerData);
 				}
-				console.log(controllerData)
-				$("div#main-result").append(html);	
+
+				console.log(controllerData);
+
+				$("div#main-result").append(html);
+
+				$("#request-pawnd-info").on("click", function(){
+
+					$(".pawned-row-button").replaceWith("<div class='row' id='small-loader'><div class='col-sm-12 col-lg'><img src='/assets/loader.gif'/></div></div>");
+					var email = $("#email").val();
+
+					$.ajax({
+						type: "POST",
+						url: "http://localhost/ajax_controller.php",
+						dataType:"json",
+						data: ({email : email, morePawndInfo : 1}),
+						cache: false,
+
+						success: function(pawndData){
+							console.log(pawndData);
+						},
+
+						complete: function(){
+							$("#small-loader").hide();
+						}
+					});					
+				});
 			},
 
 			complete: function(){
@@ -76,3 +61,77 @@ $(function() {
 		});
 	});
 });
+
+function renderResult(controllerData) {
+	var html;
+	html = "";	
+	html += "<div class='tab pawnd-tab'>";
+	html += "<div class='row website-title'>";
+		html += "<div class='col-sm site-title-text'>Has your email been compromised?</div>";
+	html += "</div>";
+
+	if (controllerData.data.have_i_been_pawned.length == false) {
+		html += "<div class='row pawnd-email-ok-message-row false-pawned'>";
+			html += "<div class='col-sm'>"
+				html += "<h5>Email Safe</h5><img src='assets/tick.svg'></img><div class='col-sm pawned-text-false'>Your email has not been found in any known data leaks</div>"
+			html += "</div>";
+		html += "</div>";
+	}else {
+
+		html += "<div class='row pawned-row'>";
+			html += "<div class='col-sm'><h5>Email Compromised!</h5><img src='assets/cross.svg'></img></div>";
+		html += "</div>";
+
+		html += "<div class='row pawned-row pawned-message'>";
+			html += "<div class='col-sm'>Your email has been found in the following data dumps</div>";	
+		html += "</div>";
+
+		$.each(controllerData.data.have_i_been_pawned, function(siteKey, siteName) {
+			html += "<div class='row pawned-row align-items-center'>";
+				html += "<div class='col-sm col-lg record pawnd-record'>" + siteName + "</div>";
+			html += "</div>";
+		});
+
+		html += "<div class='row pawned-row-button'>";
+			html += "<div class='col-sm'><div id='request-pawnd-info' class='btn btn-primary'>Find Out More</div></div>";
+		html += "</div>";						
+	}
+
+	html += "</div>";
+
+	$.each(controllerData.scrapers, function(scraperName, scraperData) {
+		if (scraperData.length == false || controllerData.scrapers.bing[1].length == false) {
+
+			html += "<div class='tab " + scraperName + "-tab'>";
+			html += "<div class='row website-title'>";
+				html += "<div class='col-sm site-title-text'>" + scraperName + "</div>";
+			html += "</div>";
+
+			html += "<div class='col-sm col-lg empty-message'>Nothing found</div>";
+			html += "</div>";
+
+		}else {
+
+			html += "<div class='tab " + scraperName + "-tab'>";
+			html += "<div class='row website-title'>";
+				html += "<div class='col-sm site-title-text'>" + scraperName + "</div>";
+			html += "</div>";
+
+			$.each(scraperData, function(pageIndex, pageData) {
+
+				$.each(pageData, function(recordIndex, record) {
+					html += "<div class='row record-row'>";
+						html += "<div class='col-sm-12 col-lg record " + scraperName + "-website-url website-url'>" + record.website + "</div>";
+						html += "<div class='col-sm-12 col-lg record " + scraperName + "-title'>" + record.title + "</div>";
+						html += "<div class='col-sm-12 col-lg record " + scraperName + "-url'><a href='" + record.url + "' target='_blank'>" + record.url.substring(0,35) + "...</a></div>";
+					html += "</div>";
+
+					html += "<hr>";
+				});
+
+			});
+			html += "</div>";
+		}
+	});
+	return html;
+}
